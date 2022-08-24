687db88f86aab3f07043e58d13aa39197649bea6
Ceyda Elmas <79998344+ceydaelmas@users.noreply.github.com>
2022-08-24 21:50:31 +0300

    ASP.Net MVC, HTML Template Integration is done.

---
+++ NortwindLvl2/MvcWebUI/Controllers/AboutController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MvcWebUI.Controllers
+{
+	public class AboutController : Controller
+	{
+
+		public IActionResult About()
+		{
+			return View();
+		}
+	}
+}
---
+++ NortwindLvl2/MvcWebUI/Controllers/ResumeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MvcWebUI.Controllers
+{
+	public class ResumeController : Controller
+	{
+		public IActionResult Resume()
+		{
+			return View();
+		}
+	}
+}
